2fe051fa822225cc390e19ce8d96e92913112d54
Qaiyyum Hakimi <qhkmdev90@gmail.com>
2020-07-14 23:08:58 +0900

    Fix old code currently not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,55 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-import { clearCurrentProfile } from "./actions/profileActions";
+/** @format */
 
-import { Provider } from "react-redux";
-import store from "./store";
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import {setCurrentUser, logoutUser} from './actions/authActions'
+import {clearCurrentProfile} from './actions/profileActions'
 
-import PrivateRoute from "./components/common/PrivateRoute";
+import {Provider} from 'react-redux'
+import store from './store'
 
-import Navbar from "./components/layouts/Navbar";
-import Footer from "./components/layouts/Footer";
-import Landing from "./components/layouts/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/create-profile/CreateProfile";
-import EditProfile from "./components/edit-profile/EditProfile";
-import AddExperience from "./components/add-credentials/AddExperience";
-import AddEducation from "./components/add-credentials/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import NotFound from "./components/not-found/NotFound";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
-import Search from "./components/search/Search";
-import Article from "./components/article/Article";
+import PrivateRoute from './components/common/PrivateRoute'
 
-import "./App.css";
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+import Landing from './components/layouts/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/create-profile/CreateProfile'
+import EditProfile from './components/edit-profile/EditProfile'
+import AddExperience from './components/add-credentials/AddExperience'
+import AddEducation from './components/add-credentials/AddEducation'
+import Profiles from './components/profiles/Profiles'
+import Profile from './components/profile/Profile'
+import NotFound from './components/not-found/NotFound'
+import Posts from './components/posts/Posts'
+import Post from './components/post/Post'
+import Search from './components/search/Search'
+import Article from './components/article/Article'
+
+import './App.css'
 
 // Check for token
 if (localStorage.jwtToken) {
   // Set auth token header auth
-  setAuthToken(localStorage.jwtToken);
+  setAuthToken(localStorage.jwtToken)
   // Decode token and get user info and exp
-  const decoded = jwt_decode(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken)
   // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
+  store.dispatch(setCurrentUser(decoded))
 
   // Check for expired token
-  const currentTime = Date.now() / 1000;
+  const currentTime = Date.now() / 1000
   if (decoded.exp < currentTime) {
     // Logout user
-    store.dispatch(logoutUser());
+    store.dispatch(logoutUser())
     // Clear current Profile
-    store.dispatch(clearCurrentProfile());
+    store.dispatch(clearCurrentProfile())
     // Redirect to login
-    window.location.href = "/login";
+    window.location.href = '/login'
   }
 }
 
@@ -68,32 +70,16 @@ class App extends Component {
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/create-profile"
-                  component={CreateProfile}
-                />
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/edit-profile"
-                  component={EditProfile}
-                />
+                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/add-experience"
-                  component={AddExperience}
-                />
+                <PrivateRoute exact path="/add-experience" component={AddExperience} />
               </Switch>
               <Switch>
-                <PrivateRoute
-                  exact
-                  path="/add-education"
-                  component={AddEducation}
-                />
+                <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/feed" component={Posts} />
@@ -109,8 +95,8 @@ class App extends Component {
           </div>
         </Router>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
